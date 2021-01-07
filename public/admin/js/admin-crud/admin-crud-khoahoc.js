fa0b476cca5fc7231c23c55a11d59fcfd3a6635b
Duy Quang <61661668+duyquang19981@users.noreply.github.com>
2021-01-08 03:51:33 +0700

    (update) delete khoa hoc

--- public/admin/js/admin-crud/admin-crud-khoahoc.js
+++ public/admin/js/admin-crud/admin-crud-khoahoc.js
@@ -43,81 +43,8 @@ $(document).ready(function(){
     console.log('delete');
     const td = $(this).closest('tr').find('td');
     const _id = td[0].innerHTML;
-    const SoKhoaHoc = +td[2].innerHTML;
     const delete_input_id = $('#delete_input_id');
     delete_input_id[0].value = _id;
-    const noti = $('.noti');
-    if(SoKhoaHoc>0){
-      noti[0].innerHTML = `There are some available courses. Category can't be deleted!`;
-      $('#deleteButton').attr('disabled', 'disabled');
-      $('.text-warning').css('display','none');
-    }
-    else{
-      noti[0].innerHTML = `Are you sure you want to delete these Records?`;
-      $('.text-warning').css('display','none');
-    }
-  });
-
-  // fill parent id for add new
-  $('.addNewSubCate').click(function(){
-    const parent_id = $(this).closest('div').find('.parent_id');
-    console.log('parent_id tm :>> ', parent_id[0].innerHTML);
-    $('#parent_id_submit')[0].value = parent_id[0].innerHTML;
-  });
-
-  $(".editButton2").click(function(){
-    console.log('edit2');
-    const td = $(this).closest('tr').find('td');
-    const _id = td[1].innerHTML;
-    const TenTheLoai = td[2].innerHTML;
-    const edit_input_id2 = $('#edit_input_id2');
-    const edit_input_ten2 = $('#edit_input_ten2');
-    edit_input_id2[0].value = _id;
-    edit_input_ten2[0].value = TenTheLoai;
-  });
-
-  $(".deleteButton2").click(function(){
-    //kiem tra o client, va server, sua o client submit nhung khong thao tac
-    console.log('delete2');
-    const td = $(this).closest('tr').find('td');
-    const parent_id = td[0].innerHTML;
-    const _id = td[1].innerHTML;
-    const SoKhoaHoc = +td[3].innerHTML;
-    console.log('parent_id :>> ', parent_id);
-    const delete_input_parent_id2 = $('#delete_input_parent_id2');
-    delete_input_parent_id2[0].value = parent_id;
-    const delete_input_id = $('#delete_input_id2');
-    delete_input_id[0].value = _id;
-    const noti = $('.noti');
-    if(SoKhoaHoc>0){
-      noti[0].innerHTML = `There are some available courses. Category can't be deleted!`;
-      $('#deleteButton2').attr('disabled', 'disabled');
-      $('.text-warning').css('display','none');
-    }
-    else{
-      noti[0].innerHTML = `Are you sure you want to delete these Records?`;
-      $('.text-warning').css('display','none');
-    }
-  });
-
-  // xem danh sach khoa hoc
-  $('.detail').click(function(){
-    console.log('xem chi tiet khoa hoc');
-    const td = $(this).closest('tr').find('td');
-    const _id = td[1].innerHTML;
-    var xhttp = new XMLHttpRequest();
-      xhttp.open('GET','TheLoai/getcoursesincate?_id=' + _id);
-    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-      xhttp.send();
-      xhttp.onreadystatechange = function(){
-          if(this.readyState == 4 && this.status == 200){
-              //alert(this.responseText);
-              var resText = this.responseText;
-              console.log('resText :>> ', resText);
-              
-          }
-
-      }
   });
 
 });
